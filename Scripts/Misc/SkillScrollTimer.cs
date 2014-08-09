8f5a386c3fe46e901659544bbb03cff219ced92a
James <james.srn@gmail.com>
2014-08-09 09:55:01 +0100

    Scroll timer
    Scrolls be droppin like candy

---
+++ Scripts/Misc/SkillScrollTimer.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using Server.Network;
+using Server;
+using Server.Items;
+using Server.Mobiles;
+
+namespace Server.Misc
+{
+	public class SkillScrollTimer : Timer
+	{
+		public static void Initialize()
+		{
+			new SkillScrollTimer().Start();
+		}
+
+		public SkillScrollTimer() : base( TimeSpan.FromHours( 24.0 ), TimeSpan.FromHours( 24.0 ) )
+		{
+			Priority = TimerPriority.OneMinute;
+		}
+
+		protected override void OnTick()
+		{
+			GiveScrollToAllPlayers();
+		}
+
+		private static void GiveScrollToAllPlayers()
+		{
+			List<Mobile> mobs = new List<Mobile>( World.Mobiles.Values );
+			
+			foreach ( Mobile m in mobs )
+			{
+				if ( m.Player ) {
+					Container pack = m.Backpack;
+					if ( pack != null )
+					{
+						pack.DropItem( new StartingSkillScroll(0.0) );
+					}
+				}
+			}
+		}
+	}
+}
