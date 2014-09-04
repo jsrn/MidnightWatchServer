9932df012dcd4a29ebd8ec181e7d177033182e6f
James <james.srn@gmail.com>
2014-09-04 22:08:39 +0100

    Back to 30

--- Scripts/Mobiles/PlayerMobile.cs
+++ Scripts/Mobiles/PlayerMobile.cs
@@ -1904,7 +1904,7 @@ namespace Server.Mobiles
 
 		public override void Resurrect()
 		{
-			if ( InjuryPoints <= 20 ) {
+			if ( InjuryPoints <= 30 ) {
 				bool wasAlive = this.Alive;
 
 				base.Resurrect();
